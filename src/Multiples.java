bca30c97bf8fa9fd0c03f17c2baeafa397de901f
Tony <tony.qi3426@gmail.com>
2024-09-09 18:22:48 -0400

    multiples method and file

---
+++ src/Multiples.java
@@ -0,0 +1,12 @@
+public class Multiples {
+
+    public static void multiples(int i) {
+        System.out.println((i / 3) + (i / 5) - (i/15));
+    }
+
+    public static void main(String[] args) {
+        multiples(1000);
+
+
+    }
+}
